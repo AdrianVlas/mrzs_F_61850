02a24c1436e64ae29ac3f82e50249c456c800c4d
Taras Malynovsky <malynovsky@ukr.net>
2019-03-28 16:16:43 +0200

    28.03.19: Вніс виправлення у константи, які відповідають за номери підверсії

--- working files/inc/a.02.c.d/const_global.h
+++ working files/inc/a.02.c.d/const_global.h
@@ -1790,7 +1790,7 @@ NUMBER_ODYNYCI_VYMIRJUVANNJA
 #define VERSIA_PZ                                             14
 #define MODYFIKACIA_VERSII_PZ                                 2
 #define ZBIRKA_VERSII_PZ                                      0
-#define ZBIRKA_PIDVERSII_PZ                                   4
+#define ZBIRKA_PIDVERSII_PZ                                   5
 /*****************************************/
 
 /*****************************************/
