1125d68fb110c32bb5e7a76e5b7acda686691c4c
Taras Malynovsky <malynovsky@ukr.net>
2019-04-19 12:05:29 +0300

    19.04.19: Оновив файли і проекти

--- working files/inc/a.01.c.d/const_global.h
+++ working files/inc/a.01.c.d/const_global.h
@@ -1778,7 +1778,7 @@ NUMBER_ODYNYCI_VYMIRJUVANNJA
 #define VERSIA_PZ                                             14
 #define MODYFIKACIA_VERSII_PZ                                 1
 #define ZBIRKA_VERSII_PZ                                      0
-#define ZBIRKA_PIDVERSII_PZ                                   6
+#define ZBIRKA_PIDVERSII_PZ                                   7
 /*****************************************/
 
 /*****************************************/
--- working files/inc/a.04.c.d/const_global.h
+++ working files/inc/a.04.c.d/const_global.h
@@ -1781,9 +1781,9 @@ NUMBER_ODYNYCI_VYMIRJUVANNJA
 //Версія програмного забезпечення
 /*****************************************/
 #define VERSIA_PZ                                             14
-#define MODYFIKACIA_VERSII_PZ                                 0
-#define ZBIRKA_VERSII_PZ                                      1
-#define ZBIRKA_PIDVERSII_PZ                                   3
+#define MODYFIKACIA_VERSII_PZ                                 4
+#define ZBIRKA_VERSII_PZ                                      0
+#define ZBIRKA_PIDVERSII_PZ                                   0
 /*****************************************/
 
 /*****************************************/
