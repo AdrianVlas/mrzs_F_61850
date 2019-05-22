8fe5e62f49f0cea3db3cce5ecbb4c72eed9d7433
Taras Malynovsky <malynovsky@ukr.net>
2019-05-22 16:19:48 +0300

    21.05.19: У файлах підправив версії ПЗ для проектів

--- working files/inc/a.00.c.d/const_global.h
+++ working files/inc/a.00.c.d/const_global.h
@@ -1782,8 +1782,8 @@ NUMBER_ODYNYCI_VYMIRJUVANNJA
 /*****************************************/
 #define VERSIA_PZ                                             14
 #define MODYFIKACIA_VERSII_PZ                                 0
-#define ZBIRKA_VERSII_PZ                                      1
-#define ZBIRKA_PIDVERSII_PZ                                   3
+#define ZBIRKA_VERSII_PZ                                      2
+#define ZBIRKA_PIDVERSII_PZ                                   0
 /*****************************************/
 
 /*****************************************/
