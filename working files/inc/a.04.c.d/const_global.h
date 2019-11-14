fe48ff9260b9c05dc089fa9b7dbdc75f1a0f595e
Taras Malynovsky <malynovsky@ukr.net>
2019-11-14 12:45:21 +0200

    14.11.19: Створена нова версія 14.11.19

--- working files/inc/a.04.c.d/const_global.h
+++ working files/inc/a.04.c.d/const_global.h
@@ -1783,7 +1783,7 @@ NUMBER_ODYNYCI_VYMIRJUVANNJA
 #define VERSIA_PZ                                             14
 #define MODYFIKACIA_VERSII_PZ                                 4
 #define ZBIRKA_VERSII_PZ                                      0
-#define ZBIRKA_PIDVERSII_PZ                                   4
+#define ZBIRKA_PIDVERSII_PZ                                   5
 /*****************************************/
 
 /*****************************************/
